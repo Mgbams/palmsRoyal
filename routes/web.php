223ee87a71bd80c11989116112a1975c7a98797c
mgbams <cmgbams@gmail.com>
2020-11-03 14:54:14 +0100

    added contents to about page

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('home', 'RoyalPalmsHomePageController@show')->name('home');
+Route::get('/', 'RoyalPalmsHomePageController@show')->name('home');
 Route::get('navigation', function () {
     return view('main-navigation');
 });
