7fa57017c58b24b8d430490ad74a10def09b5337
mgbams <cmgbams@gmail.com>
2020-11-18 15:25:21 +0100

    modified wine-list page

--- resources/views/pdf/wine-list.blade.php
+++ resources/views/pdf/wine-list.blade.php
@@ -1,14 +1,249 @@
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Wine List</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
-  </head>
-  <body>
-    <p>Wine List</p>
-  </body>
+
+<head>
+  <meta charset="utf-8">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Wine List</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <style>
+    .page_break {
+      page-break-before: always;
+    }
+  </style>
+</head>
+
+<body>
+  <div style="text-align: center;">
+    <h2>WHITES</h2>
+    <h5>LIGHT, CRISP, REFRESHING WHITES</h5>
+    <p>Chablis, Domaine du Colombier, Bourgogne, France</p>
+    <small>Beautifully balanced wine with a fruity bouquet, shades of soft yellow and apple notes.</small><br />
+    <small>A perfect pick for aperitifs and clam dishes </small><br />
+    <small style="width: 100%; margin: auto;">- $ 190 - </small>
+  </div><br />
+
+  <div style="text-align: center;">
+    <h5>Chardonnay Vintners Blend, California, USA</h5>
+    <p>This wine is ripe with aromas of pineapple, grapefruit and spices. Well-balanced, well-</p>
+    <small>structured, it offers fresh, elegant flavors with citrus, apple hints and subtle oaky notes</small><br />
+    <small style="width: 100%; margin: auto;">- $ 27 - </small>
+  </div><br />
+
+  <div style="text-align: center;">
+    <h5>Samas Isola dei Nuraghi IGT, Punica, Sardegna, Italy *</h5>
+    <p>Vermentino grape varieties in duet with barrel-aged Chardonnay impart a refined, </p>
+    <small>fresh taste and personality to this unique Sardinia-based wine</small><br />
+    <small style="width: 100%; margin: auto;">- $ 17 - </small>
+  </div><br />
+
+  <div style="text-align: center;">
+    <h5>Sauvignon Blanc, Southern Right, Eastern Cape, South Africa</h5>
+    <p>The wine’s bright individuality manifests itself in a mineral, multifaceted, deep taste and </p>
+    <small>a clean, piquant aroma. A specific terroir adds richness and a texture to classical elegance</small><br />
+    <small style="width: 100%; margin: auto;">- $ 14 - </small>
+  </div><br />
+
+  <div style="text-align: center;">
+    <h5>Gavi Masera Stefano Massone, Piedmont, Italy</h5>
+    <p>Exotic lollipop wine with a gentle, pleasant taste and smooth texture </p>
+    <small style="width: 100%; margin: auto;">- $ 19 - </small><br /><br /><br />
+    <small>* — organic wine</small>
+  </div>
+
+
+  <!--Second page-->
+  <div class="page_break">
+    <div style="text-align: center;">
+      <h2>FRUIT DRIVEN, AROMATIC WHITES</h2>
+      <h5>Viognier Vin du Pays d’Oc IGP, Delas, Vallee du Rhone, France</h5>
+      <p>Bright, aromatic wine with an intense taste of ripe peach and apricot.</p>
+      <small>A rare grape variety grown in the Southern Rhone Valley</small><br />
+      <small style="width: 100%; margin: auto;">- $ 29 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Gewurtztraminer DOC, Franz Haas, Alto Adige, Italy</h5>
+      <p>This dry white wine can easily mislead you. It is famous for its sweet aromas </p>
+      <small>and piquant, long-lasting aftertaste with notes of honey and jam</small><br />
+      <small style="width: 100%; margin: auto;">- $ 18 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Sauvignon Blanc Marlborough, Rongopai, New Zealand *</h5>
+      <p>Bright Marlborough Sauvignon with hints of gooseberry and blackcurrant leaf. </p>
+      <small>A refreshing treat, incredibly drinkable</small><br />
+      <small style="width: 100%; margin: auto;">- $ 11 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Riesling Kabinett Erdener Treppchen, Dr. Loosen, Mosel, Germany</h5>
+      <p>Golden straw-hued wine with notes of green apple, pear and meadow herbs. </p>
+      <small>It has a refreshing citrus taste to tickle your taste buds</small><br />
+      <small style="width: 100%; margin: auto;">- $ 12 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Pinot Grigio delle Venezie DOC, Borgo Molino, Veneto, Italy</h5>
+      <p>Pale straw-yellow in color, this wine is restrained on the palate with fresh herbal</p>
+      <small>flavors and a nice bitter stone fruit finish</small><br />
+      <small style="width: 100%; margin: auto;">- $ 15 - </small><br /><br />
+      <small style="width: 100%; margin: auto;">* — organic wine</small>
+    </div><br />
+  </div>
+
+  <!--Third page-->
+  <div class="page_break">
+    <div style="text-align: center;">
+      <h2>SMOOTH, SILKY WHITES</h2>
+      <h5>Grand Reserva Chardonnay, Shabo, Lyon 08, Lyon</h5>
+      <p>Light yellow champagne with a golden hue, ripe fruit aroma and touch of barrel flavor. </p>
+      <small>Grand Reserva Chardonnay is fresh, elegant and alluring</small><br />
+      <small style="width: 100%; margin: auto;">- $ 16 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Welschriesling Neusiedlersee Qualitatswein dry, Kracher,</h5>
+      <p>Burgenland, Austria * </p>
+      <small>This well-structured wine offers a notable mineral touch blended with citrus aromas.</small><br />
+      <small>It has a proper acidity level</small><br />
+      <small style="width: 100%; margin: auto;">- $ 13 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Chablis AOC, Domaine Christian Moreau, Bourgogne, France *</h5>
+      <p>Full-bodied and barrel-aged Chardonnay with distinctive mineral tones in taste </p>
+      <small>enhanced with bread notes. It has a lingering, vanilla-flavored aftertaste</small><br />
+      <small style="width: 100%; margin: auto;">- $ 11 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Riesling, Falkenstein, Sudtirol, Italy</h5>
+      <p>Riesling offers a pungent mineral note and fruity touches in taste with </p>
+      <small>an enduring yet delicate aftertaste</small><br />
+      <small style="width: 100%; margin: auto;">- $ 21 - </small><br /><br /><br />
+      <small style="width: 100%; margin: auto;">* — organic wine</small>
+    </div><br />
+  </div>
+
+  <!--Fourth page-->
+  <div class="page_break">
+    <div style="text-align: center;">
+      <h2>SMOOTH, SILKY WHITES</h2>
+      <h5>Poully-Fume La Chante des Vignes, Joseph Mellot, Val de Loire, France</h5>
+      <p>Pale yellow in color with delicate golden sparks, the wine offers a complex flavor </p>
+      <small>packed with exotic fruits, grapefruit and minerals. It tastes lively and</small><br />
+      <small>resh and has a strong, long-lasting aroma</small><br />
+      <small style="width: 100%; margin: auto;">- $ 30 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Sauvignon Ronco Del Cero, Venica&Venica, Friuli, Italy</h5>
+      <p>This intense, vigorous wine is hard to confuse with something else.</p>
+      <small>Sauvignon Ronco Del Cero has a very deep sensation and rich taste </small><br />
+      <small>with pronounced flavors of fruit puree and honey notes</small><br />
+      <small style="width: 100%; margin: auto;">- $ 24 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Sancerre AOP Silex, Fournier Pere & Fils, Vallee de Loire, France</h5>
+      <p>This unusual French Sauvignon is grown in ancient silicon-rich soil.</p>
+      <small>The taste features dominant creamy notes with a memorable vanilla aftertaste</small><br />
+      <small style="width: 100%; margin: auto;">- $ 28 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Trebbiano d’Abruzzo Gianni DOC, Masciarelli, Abruzzo, Italy</h5>
+      <p>Born in Italy’s Adriatic Coast, created by legendary winemaker Gianni Masciarelli. </p>
+      <small>Very aromatic and drinkable wine with an adequate amount of acidity</small><br />
+      <small style="width: 100%; margin: auto;">- $ 18 - </small>
+    </div><br /><br />
+  </div>
+
+  <!--Fifthth page-->
+  <div class="page_break">
+    <div style="text-align: center;">
+      <h2>FULL BODIED, RICH WHITES</h2>
+      <h5>Verdejo Fermentado en Barrica DO, Bodegas Jose Pariente, Rueda, </h5>
+      <h5>Spain</h5>
+      <p>Created by the world’s most prominent Verdejo producer, this wine has a powerful taste</p>
+      <small>owing to the method of ‘sur lie’ aging enhanced with extra barrel aging</small><br />
+      <small style="width: 100%; margin: auto;">- $ 15 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Gewurztraminer AOC Domaine Marcel Deiss, Alsace, France</h5>
+      <p>This renowned Alsace wine changes your perception of dry white wines.</p>
+      <small>Flavors of sweet rose and touches of peach jam make its taste unique</small><br />
+      <small style="width: 100%; margin: auto;">- $ 22 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Chablis Vaillons 1er Cru AOC, Domaine Christian Moreau, </h5>
+      <p>Bourgogne, France *</p>
+      <small>Benchmark Chardonnay coming from a recognized vineyard. </small><br />
+      <small>Its producer adheres to organic winemaking methods</small><br />
+      <small style="width: 100%; margin: auto;">- $ 26 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Chardonnay St-Valentin, Eppan, Sudtirol, Italy</h5>
+      <p>Full-bodied charismatic wine with a taste packed with grilled toast, nuts and vanilla.</p>
+      <small>Chardonnay St-Valentin has a rich mouthfeel of exotic fruits, butter and wood</small><br />
+      <small style="width: 100%; margin: auto;">- $ 23 - </small><br /><br /><br />
+      <small style="width: 100%; margin: auto;">* — organic wine &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; *— biodynamic wine</small>
+    </div>
+  </div>
+
+  <!--Sixth page Red wine-->
+  <div class="page_break">
+    <div style="text-align: center;">
+      <h1>REDS</h1><br /><br />
+      <h2>FRUITY, LIGHT, EASY DRINKING REDS</h2>
+      <h5>Zinfandel, Ravenswood, California, USA</h5>
+      <p>Culmination of old vines growing in Lodi’s well-drained sandy soils, this wine is explosive on </p>
+      <small>the palate. It offers a rich aroma of sweet plums, blueberries, hints of burnt spices and vanilla </small><br />
+      <small style="width: 100%; margin: auto;">- $ 14 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Gewurtztraminer DOC, Franz Haas, Alto Adige, Italy</h5>
+      <p>This dry white wine can easily mislead you. It is famous for its sweet aromas </p>
+      <small>and piquant, long-lasting aftertaste with notes of honey and jam</small><br />
+      <small style="width: 100%; margin: auto;">- $ 18 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Syrah Vin de Pays de l’Ardeche IGP Delas, Vallee du Rhone, France</h5>
+      <p>Supple, fruity red wine with violet notes and blackberry flavors in taste. </p>
+      <small>Produced out of the most important grape variety in the Rhone Valley</small><br />
+      <small style="width: 100%; margin: auto;">- $ 17 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Pinot Nero DOC, Franz Haas, Alto Adige, Italy</h5>
+      <p>The most elegant wine born in the Northern regions of Italy. It has restrained tannins</p>
+      <small>that are seamlessly blended with fruit flavors, typical of this grape variety. Franz Haas, </small><br />
+      <small>he producer, owns the highest-altitude vineyard in North Eastern Italy</small><br />
+      <small style="width: 100%; margin: auto;">- $ 20 - </small>
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Pinot Noir, Montes, Chile</h5>
+      <p>Harmonious and soft red in color. Created by top-quality producers of Chilean iconic wines</p>
+      <small style="width: 100%; margin: auto;">- $ 11 - </small><br /><br />
+    </div><br />
+
+    <div style="text-align: center;">
+      <h5>Pinot Noir, Luigi Bosca, Mendoza, Argentina</h5>
+      <p>Bright red-hued wine with black fruit and spice notes. Clear and lively,</p>
+      <small>Pinot Noir is passion-full and has an oaky finish</small><br />
+      <small style="width: 100%; margin: auto;">- $ 15 - </small>
+    </div><br />
+  </div>
+
+</body>
+
 </html>
