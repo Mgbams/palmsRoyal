d1fcd30b787de0ad767b82fa95008e8255b64bcb
mgbams <cmgbams@gmail.com>
2020-12-02 02:55:48 +0100

    correction of modal opacity

--- resources/views/admin/show/country.blade.php
+++ resources/views/admin/show/country.blade.php
@@ -92,6 +92,7 @@
 <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.22/js/dataTables.bootstrap4.min.js"></script>
 
+
 <script type="text/javascript">
     $(function() {
 
