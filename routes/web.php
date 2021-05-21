3709752a1656a577c81bff0f75874de3f8a7582e
mgbams <cmgbams@gmail.com>
2021-05-22 01:18:00 +0200

    route bug correction

--- routes/web.php
+++ routes/web.php
@@ -2,24 +2,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// ///////////////////////////////TEST ROUTES TODO
-//Route::get('senddetails', 'TestController@index');
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-/* Route::get('palms-royal-homepage', 'RoyalPalmsHomePageController@show')->name('palms-royal-homepage'); */
 Route::get('/', 'RoyalPalmsHomePageController@show')->name('palms-royal-homepage');
-/* Route::get('navigation', function () {
-    return view('main-navigation');
-}); */
 
 //return view(about-hotel)
 Route::get('about', 'AboutHotelController@show')->name('about');
@@ -61,11 +44,6 @@ Route::post('login/{locale}', 'Auth\LoginController@login');
 
 /***Register Route ***/
 Route::post('register', 'Auth\RegisterController@register');
-
-/***Logout Route ***/
-//Route::post('logout', 'Auth\LogoutController@logout');
-
-// Route::get('/home', 'HomeController@index')->name('home');
 
 //PDF download
 Route::get('/palms-royal-hotel/menu-restaurant/{menu}', 'EatAndDrinkMorningController@createPDF');
