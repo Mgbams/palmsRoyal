bba686fb4862dd534dcd212cd00d5a1cb4d9a696
mgbams <cmgbams@gmail.com>
2021-05-31 22:22:37 +0200

    modification of available rooms page and css

--- resources/views/available-rooms.blade.php
+++ resources/views/available-rooms.blade.php
@@ -9,12 +9,12 @@
     <div class="container">
         @if($available_rooms)
         @foreach($available_rooms as $room)
-        <div class="d-flex" style="width: 80vw; margin: 100px auto; background-color: #f9efea;">
-            <div class="col-md-4" style="height: 50vh; padding-left: 0px;">
+        <div class="d-flex available-room-second-container">
+            <div class="col-md-4 available-rooms-image" style="height: 50vh; padding-left: 0px;">
                 <?php $decodedImages = json_decode($room->url, true); ?>
                 <img class="w-100" src="/rooms/images/{{ $decodedImages[0] }}" style="height: 100%; width: 100%; background-size: cover;" />
             </div>
-            <div class="col-md-8 d-flex justify-content-between" style="padding-right: 0px;">
+            <div class="col-md-8 d-flex justify-content-between available-room-super-container" style="padding-right: 0px;">
                 <div>
                     <h3 class="mt-3">{{ $room->name }}</h3>
                     <div class="d-flex justify-content-between my-3">
@@ -38,11 +38,11 @@
                     </div>
                     <p class="mt-3" style="width: 250px;">{{ $room->description }}</p>
                 </div>
-                <div class="d-flex flex-column justify-content-between">
-                    <div>
+                <div class="d-flex flex-column justify-content-between available-best-rate-pay-container">
+                    <div class="available-best-rate">
                         <p><input type="radio" checked> Best available rate</p>
                     </div>
-                    <div class="d-flex align-items-end">
+                    <div class="d-flex align-items-end available-best-paynow">
                         <p class="d-flex justify-content-center align-items-center" style="background-color: #444; color:white; width: 80px; height: 50px; margin: 0px;">{{ $room->price }}&nbsp;&euro;</p>
                         <a class="btn d-flex justify-content-center align-items-center" href="{{url('/paywithpaypal/'.$room->id_room)}}" style="background-color: #A61D27; color: white !important; border-radius: 0px; height: 50px;">BOOK</a>
                     </div>
